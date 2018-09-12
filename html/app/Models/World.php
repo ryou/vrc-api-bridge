0bf85b43a0fbf29a159a2cd9e729b46b5fbe74d7
admin <visualjupiter@gmail.com>
2018-09-13 03:20:04 +0900

    Worldモデル追加

---
+++ html/app/Models/World.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\World
+ *
+ * @property int $id
+ * @property string $json
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\World whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\World whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\World whereJson($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\World whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class World extends Model
+{
+    //
+}
